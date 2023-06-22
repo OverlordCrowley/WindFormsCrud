02ac8f83b2a3c52a6124efffb35768e38712ff59
OverlordCrowley <64090766+OverlordCrowley@users.noreply.github.com>
2023-06-22 21:39:02 +0600

    Files

---
+++ Exam/postgresContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Exam
+{
+    public partial class postgresContext : DbContext
+    {
+        public postgresContext()
+        {
+        }
+
+        public postgresContext(DbContextOptions<postgresContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ChessPlayer> ChessPlayers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=postgres");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresExtension("pg_catalog", "adminpack");
+
+            modelBuilder.Entity<ChessPlayer>(entity =>
+            {
+                entity.HasKey(e => e.Идентификатор)
+                    .HasName("ChessPlayers_pkey");
+
+                entity.ToTable("ChessPlayers");
+
+                entity.Property(e => e.Звание).HasMaxLength(50);
+
+                entity.Property(e => e.Страна).HasMaxLength(50);
+
+                entity.Property(e => e.Фамилия).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
